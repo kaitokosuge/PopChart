9352fb4d8644c40daa2c82b849c57d9c82fe2799
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-11 15:35:39 +0900

    [fix]resas apiを呼ぶ関数の修正とconsoleでデータ構造の確認完了

--- src/functions/getPopByPrefecture.ts
+++ src/functions/getPopByPrefecture.ts
@@ -1,9 +1,13 @@
-export const getPopByPrefecture = async () => {
-  const res = await fetch("https://opendata.resas-portal.go.jp", {
-    headers: {
-      "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
-    },
-  });
+export const getPopByPrefecture = async (prefCode: number) => {
+  const res = await fetch(
+    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`,
+    {
+      method: "GET",
+      headers: {
+        "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
+      },
+    }
+  );
   if (!res.ok) {
     const errorText = await res.text();
     throw new Error(errorText);
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,3 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPopByPrefecture } from "../functions/getPopByPrefecture";
+
 export default function Top() {
+  const prefCode = 10;
+  const query = useQuery({
+    queryKey: ["population"],
+    queryFn: () => getPopByPrefecture(prefCode),
+  });
+  console.log(query.data);
   return <div></div>;
 }
