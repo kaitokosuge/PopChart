49632b568ca3da0175aa3a2c82dcd1bbdc9a8d9f
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-11 20:53:47 +0900

    [add]都道府県データを取得する関数の作成完了

--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPopByPrefecture } from "../functions/getPopByPrefecture";
 import TopTemplate from "../components/templates/TopTemplate";
+import { getPrefectures } from "../functions/getPrefectures";
 
 export default function Top() {
   const prefCode = 10;
@@ -9,6 +10,12 @@ export default function Top() {
     queryFn: () => getPopByPrefecture(prefCode),
   });
   console.log(query.data);
+
+  const prefData = useQuery({
+    queryKey: [`prefectures`],
+    queryFn: () => getPrefectures(),
+  });
+  console.log("prefData", prefData);
   return (
     <>
       <TopTemplate />
