56e755e02f273dd1d8e0f81251d6207d91fbddb9
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-11 20:45:21 +0900

    [fix]pref listareaマークアップ修正完了

--- src/components/organisms/PrefListArea.tsx
+++ src/components/organisms/PrefListArea.tsx
@@ -4,11 +4,11 @@ import organisms from "../../assets/css/organisms.module.css";
 
 export default function PrefListArea() {
   return (
-    <div>
+    <section>
       <Title title="都道府県" />
       <div className={organisms.pref_listarea}>
         <PrefCheckBox id="1" prefName="test" />
       </div>
-    </div>
+    </section>
   );
 }
