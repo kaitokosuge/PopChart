8d03fbc37b7816a7eb2a10e9e2a4486a6fd84306
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-11 14:54:41 +0900

    [add]App.tsxの責務をQueryClientの設定のみに変更完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import * as Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import.meta.env.RESAS_API_KEY;
+const queryClient = new QueryClient();
 
 function App() {
-  console.log(import.meta.env.VITE_RESAS_API_KEY);
-  const options = {
-    title: {
-      text: "都道府県 人口データ",
-    },
-    series: [
-      {
-        data: [10000, 15000, 13000],
-      },
-    ],
-    plotOptions: {
-      series: {
-        label: {
-          connectorAllowed: false,
-        },
-        pointRange: 10,
-        pointStart: 1970,
-      },
-    },
-  };
   return (
     <>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
     </>
   );
 }
