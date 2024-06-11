f866215016ca81e94e19461c4f9a5066af18d1b1
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-11 16:40:35 +0900

    [add]titleコンポーネントにスタイル追加完了

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,5 +1,6 @@
 import { TitleProps } from "../../types/Props";
+import atomes from "../../assets/css/atoms.module.css";
 
 export default function Title({ title }: TitleProps) {
-  return <h2>{title}</h2>;
+  return <h2 className={atomes.title}>{title}</h2>;
 }
