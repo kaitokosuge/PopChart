faea0966f81685b70c50710b183b0e27a644788b
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-13 00:26:51 +0900

    [fix]highchartのplot修正完了

--- src/components/organisms/ChartArea.tsx
+++ src/components/organisms/ChartArea.tsx
@@ -9,14 +9,45 @@ export default function ChartArea({ prefPopChartDatas }: ChartDataProps) {
       text: "都道府県 人口データ",
     },
     series: prefPopChartDatas,
+    xAxis: {
+      title: {
+        text: "年度",
+      },
+      accessibility: {
+        rangeDescription: "Range: 1960 to 2045",
+      },
+      categories: [
+        "1960",
+        "1965",
+        "1970",
+        "1975",
+        "1980",
+        "1985",
+        "1990",
+        "1995",
+        "2000",
+        "2005",
+        "2010",
+        "2015",
+        "2020",
+        "2025",
+        "2030",
+        "2035",
+        "2040",
+        "2045",
+      ],
+    },
     plotOptions: {
       series: {
         label: {
           connectorAllowed: false,
         },
-        pointRange: 10,
-        pointStart: 1970,
       },
+    },
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
     },
   };
   return (
