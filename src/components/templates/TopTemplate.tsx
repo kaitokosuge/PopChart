871e61b62a002917e84813257c10a3ebd2142ec2
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-15 12:10:33 +0900

    [add]headerスタイルの完了

--- src/components/templates/TopTemplate.tsx
+++ src/components/templates/TopTemplate.tsx
@@ -1,5 +1,6 @@
 import { TopTemplateProps } from "../../types/Props";
 import ChartArea from "../organisms/ChartArea";
+import Header from "../organisms/Header";
 import PrefListArea from "../organisms/PrefListArea";
 
 export default function TopTemplate({
@@ -7,9 +8,12 @@ export default function TopTemplate({
   prefPopChartDatas,
 }: TopTemplateProps) {
   return (
-    <main>
-      <PrefListArea prefData={prefData} />
-      <ChartArea prefPopChartDatas={prefPopChartDatas} />
-    </main>
+    <>
+      <Header />
+      <main>
+        <PrefListArea prefData={prefData} />
+        <ChartArea prefPopChartDatas={prefPopChartDatas} />
+      </main>
+    </>
   );
 }
