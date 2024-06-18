d2fe15233412731a69ce50036832b1795887cca4
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-06-18 23:35:20 +0900

    [fix]テスト文章修正

--- src/functions/__tests__/getPrefectures.test.tsx
+++ src/functions/__tests__/getPrefectures.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { getPrefectures } from "../getPrefectures";
 import { resasApiResponcePrefectures } from "./resasApiResponcePrefectures ";
 
-test("can perform API call", async () => {
+test("RESAS APIから都道府県データが本アプリにおいて必要なデータ構造で返却されるかどうか", async () => {
   const res = await getPrefectures();
   if (res.status === "success") {
     expect(res.data.result.data).toEqual(resasApiResponcePrefectures);
